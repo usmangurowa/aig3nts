17840f3f9030de6b1e654b295211aa41c085bcff
Usman Hassan <usmanhassangu@gmail.com>
2025-03-20 20:16:40 +0100

    refactor: remove unused useState import from NavBar component

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { motion, AnimatePresence } from "motion/react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
